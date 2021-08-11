681121d2978612ea57b62997b203da503ee706f4
ahlandapu <ahlandaputra001@gmail.com>
2021-08-11 11:19:29 +0700

    PuzzleABC

---
+++ app/src/main/java/com/puzzleabc/GameActivity.java
@@ -0,0 +1,172 @@
+package com.puzzleabc;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
+
+import android.content.ClipData;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.service.autofill.OnClickAction;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.RelativeLayout;
+import android.widget.Toast;
+
+import java.util.Random;
+import java.util.zip.Inflater;
+
+public class GameActivity extends AppCompatActivity {
+
+    private int emptyX=3;
+    private int emptyY=3;
+    private RelativeLayout group;
+    private Button[][] buttons;
+    private char[] tiles;
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.optionmenu, menu);
+        return true;
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_game);
+        loadViews();
+        loadLetter();
+        generateLetter();
+        loadDataToViews();
+    }
+
+    private void loadDataToViews(){
+        emptyX=3;
+        emptyY=3;
+        for (int i = 0; i < group.getChildCount() - 1; i++){
+            buttons[i/4][i%4].setText(String.valueOf(tiles[i]));
+            buttons[i/4][i%4].setBackgroundResource(android.R.drawable.btn_default);
+        }
+        buttons[emptyX][emptyY].setText("");
+        buttons[emptyX][emptyY].setBackgroundColor(ContextCompat.getColor(this,R.color.Colorfree));
+    }
+
+    private void generateLetter(){
+        int n=15;
+        Random random= new Random();
+        while (n>1){
+            int randomNum = random.nextInt(n--);
+            char temp = tiles[randomNum];
+            tiles[randomNum]=tiles[n];
+            tiles[n] = temp;
+        }
+        if(!isSolv())
+            generateLetter();
+    }
+
+    private boolean isSolv(){
+        int countInversion = 0;
+        for (int i = 0; i < 15; i++){
+            for(int j = 0; j < i; j++){
+                if(tiles[j] > tiles[i])
+                    countInversion++;
+            }
+        }
+        return countInversion%2 == 0;
+    }
+
+    private void loadLetter(){
+        tiles = new char[16];
+        for(int i = 0; i < group.getChildCount() - 1; i++){
+            tiles[i] = (char) ('A'+i);
+        }
+    }
+
+     private void loadViews(){
+        group=findViewById(R.id.group);
+        buttons = new Button[4][4];
+
+        for (int i = 0; i < group.getChildCount(); i++){
+             buttons[i/4][i%4] = (Button) group.getChildAt(i);
+         }
+    }
+
+    public void buttonClick(View view){
+        Button button = (Button) view;
+        int x = button.getTag().toString().charAt(0)-'0';
+        int y = button.getTag().toString().charAt(1)-'0';
+
+        if ((Math.abs(emptyX-x)==1&&emptyY==y)||(Math.abs(emptyY-y)==1&&emptyX==x)){
+            buttons[emptyX][emptyY].setText(button.getText().toString());
+            buttons[emptyX][emptyY].setBackgroundResource(android.R.drawable.btn_default);
+            button.setText("");
+            button.setBackgroundColor(ContextCompat.getColor(this, R.color.Colorfree));
+            emptyX = x;
+            emptyY = y;
+            checkWin();
+        }
+    }
+
+    private void checkWin() {
+        boolean isWin = false;
+        char n;
+        if (emptyX==3&&emptyY==3){
+            for (int i = 0; i < group.getChildCount() - 1; i++) {
+                n = (char) ('A' + i);
+                if (buttons[i/4][i%4].getText().toString().equals(String.valueOf(n))) {
+                    isWin=true;
+                } else {
+                    isWin=false;
+                    break;
+                }
+            }
+        }
+
+        if (isWin) {
+            Toast.makeText(this, "You Win It!!!", Toast.LENGTH_SHORT).show();
+            for (int i = 0; i < group.getChildCount(); i++) {
+                buttons[1/4][1%4].setClickable(false);
+            }
+
+        }
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.Keluar:
+                final AlertDialog.Builder builder = new AlertDialog.Builder(GameActivity.this);
+                builder.setIcon(R.drawable.ic_baseline_exit_to_app_24);
+                builder.setTitle("Confirm Exit");
+                builder.setMessage("Do you want to exit?");
+                builder.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        finish();
+                    }
+                });
+                builder.setNegativeButton("Cancel", null);
+                final AlertDialog alertDialog = builder.create();
+                alertDialog.show();
+                break;
+            case R.id.Acak:
+                if (item.getItemId() == R.id.Acak){
+                    loadViews();
+                    loadLetter();
+                    generateLetter();
+                    loadDataToViews();
+                }else {
+                    finish();
+                }
+                return super.onOptionsItemSelected(item);
+        }
+        return true;
+    }
+}
---
+++ app/src/main/java/com/puzzleabc/MainActivity.java
@@ -0,0 +1,44 @@
+package com.puzzleabc;
+
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button but;
+
+    @Override
+    public void onBackPressed() {
+        final AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+        builder.setIcon(R.drawable.ic_baseline_exit_to_app_24);
+        builder.setTitle("Exit App...");
+        builder.setMessage("Thank you for playing this game! :D");
+        builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                finish();
+            }
+        });
+        final AlertDialog alertDialog = builder.create();
+        alertDialog.show();
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        but = findViewById(R.id.startgame);
+        but.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this, GameActivity.class));
+            }
+        });
+    }
+}
